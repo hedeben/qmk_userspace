a3fa5a7a3abd073d8a18bfe6acac09aa4215843c
Benjamin Hede <benhede@pm.me>
2024-08-05 22:35:22 +0200

    RGB&OLED

--- keyboards/crkbd/keymaps/swolemak/keymap.c
+++ keyboards/crkbd/keymaps/swolemak/keymap.c
@@ -13,11 +13,59 @@ enum layer_names {
 enum custom_keycodes {
     CMDH = SAFE_RANGE,
     GRAPH,
-    FNNUM,
+    FNUM,
     SYM,
     NAV,
-    RGBOPT
+    RGBO
 };
+
+#ifdef OLED_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return OLED_ROTATION_270;
+}
+
+// #include "logo.c"
+#include "ow.c"
+
+bool oled_task_user(void) {
+  if (!is_keyboard_master()) {
+    oled_set_cursor(0, 1);
+    oled_write_raw_P(solar, sizeof(solar)); 
+    // render_animation();
+  } else {
+    // oled_render_logo();
+    // Switch on current active layer
+    oled_set_cursor(0, 1);
+    switch (get_highest_layer(layer_state)) {
+        case _COLEMAK_DH :
+            oled_write("C-DH", false);
+            break;
+        case _GRAPHITE :
+            oled_write("Graph", false);
+            break;
+        case _SYMBOLS :
+            oled_write("Sym", false);
+            break;
+        case _FNNUM : 
+            oled_write("Func", false);
+            break;
+        case _NAV :
+            oled_write("Nav", false);
+            break;
+        case _RGB : 
+            oled_write("RGB", false);
+            break;
+    }
+    oled_write("\n\n", false);
+    // LED status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write("\n", false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAPS") : PSTR("    "), false);
+  }
+  return false;
+}
+#endif
 
 // SE_SECT // §
 // SE_CURR // ¤
@@ -27,7 +75,7 @@ enum custom_keycodes {
 // SE_MICR // µ
 
 #define SE_DEG RALT(LSFT(KC_0)) // °
-#define SE_NOT RALT(SE_ACUT)    // ¬
+// #define SE_NOT RALT(SE_ACUT)    // ¬
 
 #define ESC_C LCTL_T(KC_ESC)    // Esc on tap, Ctrl on hold
 // #define DEL_SYM LT(SYM,KC_DEL)
@@ -46,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LALT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, SE_MINS, KC_LGUI,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                              SYM, KC_LSFT, KC_BSPC,     KC_ENT, SPC_NAV, FNNUM
+                                          MO(SYM), KC_LSFT, KC_BSPC,     KC_ENT, SPC_NAV, MO(FNUM)
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -58,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LALT,    KC_Q,    KC_X,    KC_M,    KC_C,    KC_V,                         KC_K,    KC_P, KC_COMM,  KC_DOT, SE_MINS, KC_LGUI,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                              SYM, KC_LSFT, KC_BSPC,     KC_ENT, SPC_NAV, FNNUM
+                                          MO(SYM), KC_LSFT, KC_BSPC,     KC_ENT, SPC_NAV, MO(FNUM)
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -66,23 +114,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_PSCR,                      XXXXXXX,   KC_P7,   KC_P8,   KC_P9, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_RCTL,   KC_F5,   KC_F6,   KC_F7,   KC_F8, KC_PAUS,                        GRAPH,   KC_P4,   KC_P5,   KC_P6,   KC_P0, XXXXXXX,
+      KC_RCTL,   KC_F5,   KC_F6,   KC_F7,   KC_F8, KC_PAUS,                     DF(GRAPH),  KC_P4,   KC_P5,   KC_P6,   KC_P0, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_RALT,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_INS,                         CMDH,   KC_P1,   KC_P2,   KC_P3,  KC_APP, _______,
+      KC_RALT,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_INS,                     DF(CMDH),   KC_P1,   KC_P2,   KC_P3,  KC_APP, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           RGBOPT, KC_CAPS,  KC_DEL,    _______,  KC_NUM, _______
+                                         MO(RGBO), KC_CAPS,  KC_DEL,    _______,  KC_NUM, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
     [_SYMBOLS] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, SE_BSLS, SE_SLSH, SE_QUOT, SE_DQUO, SE_TILD,                      SE_DIAE, KC_PERC, KC_HASH, SE_CIRC, SE_PLUS, SE_DEG,
+      XXXXXXX, SE_BSLS, SE_SLSH, SE_QUOT, SE_DQUO, SE_TILD,                      SE_DIAE, KC_PERC, KC_HASH, SE_CIRC, SE_PLUS,  SE_DEG,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, SE_LBRC, SE_RBRC, SE_LPRN, SE_RPRN,  SE_GRV,                      SE_PIPE, KC_EXLM, SE_AMPR, SE_ASTR, SE_EQL, SE_DLR,
+      _______, SE_LBRC, SE_RBRC, SE_LPRN, SE_RPRN,  SE_GRV,                      SE_PIPE, KC_EXLM, SE_AMPR, SE_ASTR,  SE_EQL,  SE_DLR,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, SE_LABK, SE_RABK, SE_LCBR, SE_RCBR, SE_ACUT,                        SE_AT, SE_QUES, SE_SCLN, SE_COLN, SE_UNDS, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______,  KC_DEL,    XXXXXXX, XXXXXXX,  RGBOPT
+                                          _______, _______,  KC_DEL,    XXXXXXX, XXXXXXX,MO(RGBO)
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -111,37 +159,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case CMDH:
-            if (record->event.pressed) {
-                set_single_persistent_default_layer(_COLEMAK_DH);
-            }
-            return false;
-            break;
-        case GRAPH:
-            if (record->event.pressed) {
-                set_single_persistent_default_layer(_GRAPHITE);
-            }
-            return false;
-            break;
-        case SYM:
-            (record->event.pressed) ? layer_on(_SYMBOLS) : layer_off(_SYMBOLS);
-            return false;
-            break;
-        case FNNUM:
-            (record->event.pressed) ? layer_on(_FNNUM) : layer_off(_FNNUM);
-            return false;
-            break;
-        case NAV:
-            (record->event.pressed) ? layer_on(_NAV) : layer_off(_NAV);
-            return false;
-            break;
-        case RGBOPT:
-            (record->event.pressed) ? layer_on(_RGB) : layer_off(_RGB);
-            return false;
-            break;
-    }
-    return true;
-}
--- keyboards/crkbd/keymaps/swolemak/rules.mk
+++ keyboards/crkbd/keymaps/swolemak/rules.mk
@@ -1,1 +1,6 @@
 BOOTLOADER = atmel-dfu
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+# WPM_ENABLE = yes  # Enable word per minute counter
+LTO_ENABLE = yes  # Makes hex file smaller
+OLED_ENABLE = yes # Enable OLEDs
